7af8d24b27a6e158bd6a7b9866ffc0213a50e999
Raian-Moretti <raian.moretti.eletro@gmail.com>
2022-09-10 11:36:39 -0300

    fix: integral_img

--- Project-2--Blur/main.py
+++ Project-2--Blur/main.py
@@ -9,11 +9,12 @@ import sys
 import timeit
 import numpy as np
 import cv2
+import collections
 
 #===============================================================================
 
-INPUT_IMAGE =  'chave.bmp'
-TAMANHO_JANELA = -10, 10
+INPUT_IMAGE =  'eyehand.bmp'
+TAMANHO_JANELA = -20, 20
 
 
 
@@ -80,30 +81,46 @@ def cria_integral_da_imagem (img):
     img_integral = np.zeros_like (img)
     size_y = img.shape[0]
     size_x = img.shape[1]
-
+    
     for y in range (size_y):
+        img_integral[y,0] = img[y,0]
+        for x in range (1, size_x):
+            img_integral[y,x] = img[y,x] + img_integral[y,x-1]
+            
+    for y in range (1, size_y):
         for x in range (size_x):
-            for (x, y) in ((x, y), (x, y-1), (x-1, y), (x-1, y-1)):
-                if esta_dentro_da_imagem(img_integral.shape, y, x):
-                    img_integral [y, x] += img [y, x]
+            img_integral[y,x] += img_integral[y-1,x]
+    
     return img_integral
 
 def blur_imagem_integral (img):
     img_out = np.zeros_like (img)
     size_y = img.shape[0]
     size_x = img.shape[1]
-
     img_integral = cria_integral_da_imagem(img)
 
-    for y in range (size_y):
-        for x in range (size_x):
-            soma = 0
-            num_pixels_janela = 0
-            for janela_y, janela_x in [(y, x), (TAMANHO_JANELA[0], x), (y, TAMANHO_JANELA[1]), (TAMANHO_JANELA[0], TAMANHO_JANELA[1])]:
-                if esta_dentro_da_imagem(img_integral.shape, y + janela_y, x + janela_x):
-                    soma += img_integral [y + janela_y, x + janela_x]
-                    num_pixels_janela += 1
-            img_out [y, x] = soma / num_pixels_janela
+    janela = TAMANHO_JANELA[1]
+    for y in range (int(size_y)):
+        sum=0
+        for x in range (int(size_x)):
+            if(TAMANHO_JANELA[1] > x or TAMANHO_JANELA[1] > y):
+                janela = x if x < y else y
+                if((not(esta_dentro_da_imagem(img.shape, y+janela, x))) and y+janela > x+janela):
+                    sum+=1
+                    janela-=sum
+                elif((not(esta_dentro_da_imagem(img.shape, y, x+janela))) and x+janela > y+janela):
+                    sum+=1
+                    janela-=sum
+            else:
+                if((not(esta_dentro_da_imagem(img.shape, y+janela, x))) and y+janela > x+janela):
+                    janela-=1
+                elif((not(esta_dentro_da_imagem(img.shape, y, x+janela))) and x+janela > y+janela):
+                    janela-=1
+            if(janela!=0):    
+                img_out[y][x] = (img_integral[y+janela][x+janela] - img_integral[y-janela][x+janela] - img_integral[y+janela][x-janela] + img_integral[y-janela][x-janela])/(janela*janela*4)
+            else:
+                img_out[y][x] = img[y][x]
+
     return img_out
 
 #===============================================================================
@@ -131,7 +148,7 @@ def main ():
 
     start_time = timeit.default_timer ()
     img_out = blur_imagem_integral (img)
-    img_out2 = cv2.blur (img, (20, 20))
+    img_out2 = cv2.blur (img, (40, 40))
     print ('Tempo: %f' % (timeit.default_timer () - start_time))
 
     cv2.imshow ('02 - out', img_out)
