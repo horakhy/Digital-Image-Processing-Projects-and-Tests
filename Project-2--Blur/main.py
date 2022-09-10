6de05dae430a2d917c75eb634954d6b6641b1f06
Raian-Moretti <raian.moretti.eletro@gmail.com>
2022-09-10 12:49:10 -0300

    feat: calculate rgb channels in integral image

--- Project-2--Blur/main.py
+++ Project-2--Blur/main.py
@@ -13,8 +13,8 @@ import collections
 
 #===============================================================================
 
-INPUT_IMAGE =  'flowers.bmp'
-TAMANHO_JANELA = -10, 10
+INPUT_IMAGE =  'chave.bmp'
+TAMANHO_JANELA = -40, 40
 
 
 
@@ -88,15 +88,17 @@ def cria_integral_da_imagem (img):
     img_integral = np.zeros_like (img)
     size_y = img.shape[0]
     size_x = img.shape[1]
+    channels = img.shape[2]
     
-    for y in range (size_y):
-        img_integral[y,0] = img[y,0]
-        for x in range (1, size_x):
-            img_integral[y,x] = img[y,x] + img_integral[y,x-1]
-            
-    for y in range (1, size_y):
-        for x in range (size_x):
-            img_integral[y,x] += img_integral[y-1,x]
+    for z in range (channels):
+        for y in range (size_y):
+            img_integral[y,0, z] = img[y,0, z]
+            for x in range (1, size_x):
+                img_integral[y,x, z] = img[y,x, z] + img_integral[y,x-1, z]
+          
+        for y in range (1, size_y):
+            for x in range (size_x):
+                img_integral[y,x, z] += img_integral[y-1,x, z]
     
     return img_integral
 
@@ -104,29 +106,32 @@ def blur_imagem_integral (img):
     img_out = np.zeros_like (img)
     size_y = img.shape[0]
     size_x = img.shape[1]
+    channels = img.shape[2]
+
     img_integral = cria_integral_da_imagem(img)
 
-    janela = TAMANHO_JANELA[1]
-    for y in range (int(size_y)):
-        sum=0
-        for x in range (int(size_x)):
-            if(TAMANHO_JANELA[1] > x or TAMANHO_JANELA[1] > y):
-                janela = x if x < y else y
-                if((not(esta_dentro_da_imagem(img.shape, y+janela, x))) and y+janela > x+janela):
-                    sum+=1
-                    janela-=sum
-                elif((not(esta_dentro_da_imagem(img.shape, y, x+janela))) and x+janela > y+janela):
-                    sum+=1
-                    janela-=sum
-            else:
-                if((not(esta_dentro_da_imagem(img.shape, y+janela, x))) and y+janela > x+janela):
-                    janela-=1
-                elif((not(esta_dentro_da_imagem(img.shape, y, x+janela))) and x+janela > y+janela):
-                    janela-=1
-            if(janela!=0): 
-                img_out[y][x] = (img_integral[y+janela][x+janela] - img_integral[y-janela][x+janela] - img_integral[y+janela][x-janela] + img_integral[y-janela][x-janela])/(janela*janela*4)
-            else:
-                img_out[y][x] = img[y][x]
+    for z in range (channels):
+        janela = TAMANHO_JANELA[1]
+        for y in range (int(size_y)):
+            sum=0
+            for x in range (int(size_x)):
+                if(TAMANHO_JANELA[1] > x or TAMANHO_JANELA[1] > y):
+                    janela = x if x < y else y
+                    if((not(esta_dentro_da_imagem(img.shape, y+janela, x))) and y+janela > x+janela):
+                        sum+=1
+                        janela-=sum
+                    elif((not(esta_dentro_da_imagem(img.shape, y, x+janela))) and x+janela > y+janela):
+                        sum+=1
+                        janela-=sum
+                else:
+                    if((not(esta_dentro_da_imagem(img.shape, y+janela, x))) and y+janela > x+janela):
+                        janela-=1
+                    elif((not(esta_dentro_da_imagem(img.shape, y, x+janela))) and x+janela > y+janela):
+                        janela-=1
+                if(janela!=0): 
+                    img_out[y][x][z] = (img_integral[y+janela][x+janela][z] - img_integral[y-janela][x+janela][z] - img_integral[y+janela][x-janela][z] + img_integral[y-janela][x-janela][z])/(janela*janela*4)
+                else:
+                    img_out[y][x][z] = img[y][x][z]
 
     return img_out
 
@@ -155,7 +160,7 @@ def main ():
 
     start_time = timeit.default_timer ()
     img_out = blur_imagem_integral (img)
-    img_out2 = cv2.blur (img, (20, 20))
+    img_out2 = cv2.blur (img, (80, 80))
     print ('Tempo: %f' % (timeit.default_timer () - start_time))
 
     cv2.imshow ('02 - out', img_out)
