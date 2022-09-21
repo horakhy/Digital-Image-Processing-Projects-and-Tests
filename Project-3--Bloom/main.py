3e9dc23a7795cca6db109d8df61e8264f5339479
horakhy <leonidas51000@hotmail.com>
2022-09-20 22:21:12 -0300

    feat: bloom effect working

--- Project-3--Bloom/main.py
+++ Project-3--Bloom/main.py
@@ -13,24 +13,32 @@ import cv2
 #===============================================================================
 
 INPUT_IMAGE =  'GT2.BMP'
-THRESHOLD = 0.6
-UPPER_LIMIT = 2
-SIGMA = 7
-KERNEL = 7
+SIGMA_LIMIT = 5
+KERNEL = 5
+SIGMA_MULTIPLIER = 2
+BLUR_SUM_COUNTER = SIGMA_LIMIT
 
 def gaussianBlur(img):
-    sigma = 1
-    for sigma in range(SIGMA):
-        img += cv2.GaussianBlur(img, (KERNEL,KERNEL), sigma)
-        sigma*=2
+    sigma = SIGMA_MULTIPLIER
+    img_to_be_blurred = img.copy()
+
+    for sigma in range(SIGMA_LIMIT):
+        img_to_be_blurred += cv2.GaussianBlur(img_to_be_blurred, (KERNEL,KERNEL), sigma)
+        sigma*=SIGMA_MULTIPLIER
     
-    return img
+    return img_to_be_blurred
 
 def boxBlur(img):
-    for x in range(SIGMA):
-        img += cv2.blur(img, (KERNEL,KERNEL))
+    img_to_be_blurred = img.copy()
+    for x in range(BLUR_SUM_COUNTER):
+        img_to_be_blurred += cv2.blur(img_to_be_blurred, (KERNEL,KERNEL))
         x+=1
-    return img
+    return img_to_be_blurred
+
+def merge_img(img, blurred_bright_pass, alpha):
+    
+    return (img*alpha)+(blurred_bright_pass*(1 - alpha))
+
 #===============================================================================
 
 def main ():
@@ -41,19 +49,33 @@ def main ():
         sys.exit ()
     img = img.reshape ((img.shape [0], img.shape [1], 3))
 
-    # # Apenas o canal de luminosidade
+    img = img.astype (np.float32) / 255.0
+    alpha = 0.95
+
+    ## Apenas o canal de luminosidade
     lightness = img[:,:,1]
-    # # Criando imagem da máscara contendo as fontes de luz
-    light_mask = cv2.inRange(lightness, 127, 255)
+    
+    ## Criando imagem da máscara contendo as fontes de luz
+    light_mask = cv2.inRange(lightness, 0.5, 1.0)
 
     start_time = timeit.default_timer()
     
-    img = cv2.bitwise_and(img, img, mask = light_mask)
-    img_out = boxBlur(img)
+    img_bright_pass = cv2.bitwise_and(img, img, mask = light_mask)
+
+    blurred_bright_pass = gaussianBlur(img_bright_pass)
+    img_out = merge_img(img, blurred_bright_pass, alpha)
     print("Tempo: %f" % (timeit.default_timer() - start_time))
 
-    cv2.imshow("02 - out", img_out)
-    cv2.imwrite("02 - out.png", img_out * 255)
+    # cv2.imshow("01 - light_mask", light_mask)
+    # cv2.imwrite("01 - light_mask.png", light_mask)
+    # cv2.imshow("02 - img_bright_pass", img_bright_pass)
+    # cv2.imwrite("02 - img_bright_pass.png", img_bright_pass * 255)
+    # cv2.imshow("03 - blurred_bright_pass", blurred_bright_pass)
+    # cv2.imwrite("03 - blurred_bright_pass.png", blurred_bright_pass * 255)
+    cv2.imshow("04 - img_original", img)
+    cv2.imwrite("04 - img_original.png", img * 255)
+    cv2.imshow("05 - img_out", img_out)
+    cv2.imwrite("05 - img_out.png", img_out * 255)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
