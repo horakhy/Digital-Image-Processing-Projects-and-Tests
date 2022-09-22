d295d68fe5100e6cb4c27af0ec890e7883157340
horakhy <leonidas51000@hotmail.com>
2022-09-21 22:00:28 -0300

    A few adjustments

--- Project-3--Bloom/main.py
+++ Project-3--Bloom/main.py
@@ -14,14 +14,16 @@ import cv2
 
 INPUT_IMAGE =  'GT2.BMP'
 SIGMA_LIMIT = 5
-KERNEL = 5
+KERNEL = 15
 SIGMA_MULTIPLIER = 2
-BLUR_SUM_COUNTER = SIGMA_LIMIT
+BLUR_SUM_COUNTER = SIGMA_LIMIT 
+ALPHA = 0.95
 
 def gaussianBlur(img):
     sigma = SIGMA_MULTIPLIER
     img_to_be_blurred = img.copy()
 
+    ## sigma cresce como PG
     for sigma in range(SIGMA_LIMIT):
         img_to_be_blurred += cv2.GaussianBlur(img_to_be_blurred, (KERNEL,KERNEL), sigma)
         sigma*=SIGMA_MULTIPLIER
@@ -30,14 +32,16 @@ def gaussianBlur(img):
 
 def boxBlur(img):
     img_to_be_blurred = img.copy()
-    for x in range(BLUR_SUM_COUNTER):
+
+    # x cresce como PA
+    for counter in range(BLUR_SUM_COUNTER):
         img_to_be_blurred += cv2.blur(img_to_be_blurred, (KERNEL,KERNEL))
-        x+=1
+        counter+=1
     return img_to_be_blurred
 
-def merge_img(img, blurred_bright_pass, alpha):
+def merge_img(img, blurred_bright_pass):
     
-    return (img*alpha)+(blurred_bright_pass*(1 - alpha))
+    return (img * ALPHA)+(blurred_bright_pass * (1 - ALPHA))
 
 #===============================================================================
 
@@ -50,7 +54,6 @@ def main ():
     img = img.reshape ((img.shape [0], img.shape [1], 3))
 
     img = img.astype (np.float32) / 255.0
-    alpha = 0.95
 
     ## Apenas o canal de luminosidade
     lightness = img[:,:,1]
@@ -62,9 +65,13 @@ def main ():
     
     img_bright_pass = cv2.bitwise_and(img, img, mask = light_mask)
 
-    blurred_bright_pass = gaussianBlur(img_bright_pass)
-    img_out = merge_img(img, blurred_bright_pass, alpha)
+    ## Alternar entre gaussianBlur e boxBlur
+    blurred_bright_pass = boxBlur(img_bright_pass)
+
+    img_out = merge_img(img, blurred_bright_pass)
     print("Tempo: %f" % (timeit.default_timer() - start_time))
+
+    ### Imagens intermediárias para visualização
 
     # cv2.imshow("01 - light_mask", light_mask)
     # cv2.imwrite("01 - light_mask.png", light_mask)
@@ -72,6 +79,7 @@ def main ():
     # cv2.imwrite("02 - img_bright_pass.png", img_bright_pass * 255)
     # cv2.imshow("03 - blurred_bright_pass", blurred_bright_pass)
     # cv2.imwrite("03 - blurred_bright_pass.png", blurred_bright_pass * 255)
+
     cv2.imshow("04 - img_original", img)
     cv2.imwrite("04 - img_original.png", img * 255)
     cv2.imshow("05 - img_out", img_out)
