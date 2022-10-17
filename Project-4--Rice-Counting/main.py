f334c0dea684da8a317eb16e48ed6ade964d4888
Christian A. Carneiro <leonidas51000@hotmail.com>
2022-10-16 21:09:23 -0300

    Update main.py

--- Project-4--Rice-Counting/main.py
+++ Project-4--Rice-Counting/main.py
@@ -1,56 +1,83 @@
-import sys
-import timeit
-import numpy as np
 import cv2
-import matplotlib.pyplot as plt
+import sys
+import numpy as np
+import math
 
-NEGATIVO = False
-THRESHOLD = 0.8
-ALTURA_MIN = 5
-LARGURA_MIN = 5
-N_PIXELS_MIN = 5
+path = '150.bmp'
 
-INPUT_IMAGE = '82.bmp'
+def processing(img):
+ img_out = img.copy()
+ kernel = np.ones((3,3),np.uint8)
 
-## flood fill and count the number of pixels
-def flood_fill(image, x, y, color):
-  if image[y, x] == 0:
-    image[y, x] = color
-    flood_fill(image, x + 1, y, color)
-    flood_fill(image, x - 1, y, color)
-    flood_fill(image, x, y + 1, color)
-    flood_fill(image, x, y - 1, color)
-    
+ img_out = cv2.adaptiveThreshold(img_out,255,cv2.ADAPTIVE_THRESH_MEAN_C,
+            cv2.THRESH_BINARY,25,-20)  
+ cv2.imwrite ('01 - Adaptative_Threshold.png', img_out)
 
-def main():
+ img_out  = cv2.morphologyEx(img_out,cv2.MORPH_OPEN,kernel,iterations=1) 
+ cv2.imwrite ('02 - Morphology.png', img_out)
 
-  image = cv2.imread(INPUT_IMAGE) 
-  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
-  edged = cv2.Canny(gray, 80, 200) 
-  
-  # edged = cv2.GaussianBlur(edged, (3, 3), 0)
-  
-  contours, hierarchy = cv2.findContours(edged,  
-      cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
-  amount = 0
-  
-  ## draw all contours with a minimum area of 60 pixels
-  ## and a minimum width of 10 pixels
-  for c in contours:
-    if cv2.contourArea(c) > N_PIXELS_MIN:
-      x, y, w, h = cv2.boundingRect(c)
-      if w > LARGURA_MIN and h > ALTURA_MIN:
-        amount += 1
-        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+ return img_out
 
-  cv2.imshow('Canny Edges After Contouring', edged) 
+image = cv2.imread(path, 0)
+if image is None:
+        print ('Erro abrindo a imagem.\n')
+        sys.exit ()
 
-  print("Number of Contours found = " + str(amount)) 
-  cv2.drawContours(image, contours, -1, (0, 255, 0), 2) 
+processed = processing(image)
 
-  cv2.imshow('Contours', image) 
-  cv2.waitKey(0) 
-  cv2.destroyAllWindows() 
+(cnts, hierarchy) = cv2.findContours(processed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+totalArea = 0.0
+listArea = []
+print(' lista de contornos:', len(cnts))
+for x in range(len(cnts)):
+        listArea.append(cv2.contourArea(cnts[x]))
+        totalArea += listArea[x]
 
-if __name__ == "__main__":
-    main()
+listAreaSorted = sorted(listArea)
+print('lista de areas:', listAreaSorted)
+print('area total: ', totalArea)
+finalList = []
+somaMenores = listAreaSorted[0] 
+for x in range(len(listAreaSorted)):
+        if(somaMenores < totalArea*(0.01)):
+               somaMenores+=listAreaSorted[x] 
+               continue
+        finalList = listAreaSorted[x-1:]
+        break
+somaListaFinal = 0.0
+for x in range(len(finalList)):
+        somaListaFinal+= finalList[x]
+
+somaMaiores = finalList[len(finalList)-1] 
+for x, item in reversed(list(enumerate(finalList))):
+        if somaMaiores < totalArea*(0.02):
+                somaMaiores+=finalList[x]
+                continue
+        finalList = finalList[0:x]
+        break
+        
+# somaListaFinal = 0.0
+# for x in range(len(finalList)):
+
+#         somaListaFinal+= finalList[x]
+
+print('lista de areas final:', finalList)
+
+mean = (totalArea-(somaMenores+somaMaiores)) / len(finalList)
+cont = 0.0
+for x in range(len(finalList)):
+        if(finalList[x] < mean*0.2):
+                continue
+        if(finalList[x] > mean):
+                cont+=round(finalList[x]/mean) 
+                continue
+        cont+=1
+
+rices = cont
+
+cv2.drawContours(rgb, cnts, -1, (0,0,255), 2)
+cv2.imwrite ('03 - Draw.png', rgb)
+rices = math.ceil(rices)
+print('Rice in the image: ', rices)
+#cv2.show()
